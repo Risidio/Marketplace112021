5522c6325050b3bc93d69d3be11993f4a6305b75
Bara Konateh <bkonateh1@gmail.com>
2021-12-21 16:03:48 +0000

    Update Launch Collection

--- src/components/launchCollection/LaunchCollection1.vue
+++ src/components/launchCollection/LaunchCollection1.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="launchS1">
-            <!-- <img alt="background" src="https://res.cloudinary.com/risidio/image/upload/v1640097201/RisidioMarketplace/pexels-photo-1183992_hbs27i.jpg"/> -->
             <img class="background" src="https://res.cloudinary.com/risidio/image/upload/v1640097610/RisidioMarketplace/pexels-photo-1183992_myd1vf.jpg" alt="">
+            <div class="icons">
+                <img class="icon" src="https://res.cloudinary.com/risidio/image/upload/v1640097848/RisidioMarketplace/facebook_ls2dko.svg"/>
+                <img class="icon" src="https://res.cloudinary.com/risidio/image/upload/v1640102430/RisidioMarketplace/twitter-black-shape-svgrepo-com_pfxicm.svg"/>
+                <img class="icon" src="https://res.cloudinary.com/risidio/image/upload/v1640102517/RisidioMarketplace/instagram-svgrepo-com_apkdhy.svg"/>
+            </div>
         <div class = "container">
             <img alt="Collection Image" src="https://res.cloudinary.com/risidio/image/upload/v1640003055/RisidioMarketplace/Tiger_BitStacks_By_Benny_d8rprd.png" class="circleImage"/>
             <h1 class="collectionName">{{content.intro[0].title[0].text}}</h1>
@@ -27,9 +31,9 @@ export default {
     margin-bottom: 50px;
 }
 .container{
-    margin-top: 50px;
     text-align: center;
     max-width: 600px;
+    margin-top: 80px;
 }
 .collectionName{
     font: normal normal 300 40px/55px Montserrat;
@@ -48,8 +52,8 @@ export default {
     padding: 5px;
 }
 .circleImage{
-    width: 250px;
-    height: 250px;
+    width: 200px;
+    height: 200px;
     border-radius: 50%;
     box-shadow: 10px 10px 30px #0000002F;
 }
@@ -58,11 +62,26 @@ export default {
   left: 0;
   top: 100px;
   width: 100%;
-  height: 40rem;
+  height: 41rem;
   object-fit: cover;
   z-index: -10;
   transform: rotate(180deg);
   margin-bottom: 50px;
   opacity: 10%;
+}
+.icons{
+    position: absolute;
+    margin-left: auto;
+    right: 80px;
+    top: 250px;
+}
+.icon{
+    margin: 0 10px;
+    width: 30px;
+    height: 25px;
+    cursor: pointer
+}
+.icon:hover{
+    transform: scale(1.1)
 }
 </style>
