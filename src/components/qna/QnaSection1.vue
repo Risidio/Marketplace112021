5af18ddc461f1e38339ac039180a8e8b6242ffc7
Camila Tavares <87905223+Camilatavaresg@users.noreply.github.com>
2021-12-29 16:25:38 +0000

    qna page
    qna

---
+++ src/components/qna/QnaSection1.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="qnasection1">
+        <div class="qnacontainer">
+            <h1>Join our Competition</h1>
+            <p>Catch phrase</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem omnis quidem, quo ipsa repellendus officia repudiandae earum optio, deleniti beatae, totam tenetur reiciendis fugiat pariatur numquam qui alias accusamus distinctio!</p>
+            <button>Participate</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+    .qnasection1 {
+        width: 100%;
+        min-height: 500px;
+        text-align: center;
+    }
+    .qnacontainer {
+        max-width: 868px;
+        margin: auto;
+        margin-top: 200px;
+
+        h1{
+            font: normal normal 300 40px/55px Montserrat;
+        }
+
+        button {
+            padding: 12px 35px;
+            color: rgba(95, 189, 193, 1);
+            background: rgba(95, 189, 193, 0.1);
+            border: none;
+            border-radius: 100px;
+            font-size: 11px;
+            font-weight: 500;
+        }
+        }
+</style>
--- src/views/QandA.vue
+++ src/views/QandA.vue
@@ -1,9 +1,14 @@
 <template>
-
+    <QnaSection1></QnaSection1>
 </template>
 
 <script>
-
+import QnaSection1 from '@/components/qna/QnaSection1.vue'
+export default {
+  components: {
+    QnaSection1
+  }
+}
 </script>
 
 <style lang="scss" scoped>
