9b16dd7e495e76ddba013fee4ccad3c4cef43132
Camila Tavares <87905223+Camilatavaresg@users.noreply.github.com>
2021-12-30 12:58:13 +0000

    qna section
    Completed

--- src/components/qna/QnaSection1.vue
+++ src/components/qna/QnaSection1.vue
@@ -3,8 +3,20 @@
         <div class="qnacontainer">
             <h1>Join our Competition</h1>
             <p>Catch phrase</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem omnis quidem, quo ipsa repellendus officia repudiandae earum optio, deleniti beatae, totam tenetur reiciendis fugiat pariatur numquam qui alias accusamus distinctio!</p>
-            <button>Participate</button>
+            <p class="qna-p">(Exmanation / rulles ) <br> Lorem ipsum dolor sit amet, consetetu. Storem ipsum dolore magna. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua</p>
+            <button class="button notFilledBlue">Participate</button>
+        </div>
+        <div class="qna-questions">
+            <!-- <button class="qna-arrow"></button> -->
+            <div class="qna-p-container">
+                <h1>QnA question</h1>
+                <div class="qna-p">
+                    <p>(Little explanation on what it is to min an item)</p>
+                    <p>You can mint your own NFT on Risidio Marletplace. All you need to do is to make sure you are connecter, go to your profil, and clique on the «+» sine. You will lode a file and get all the infos. Once its done, you are ready to min. Little bonus, you can set Royalties you will get every time your artwork is resell.</p>
+                </div>
+                <button class="button filled qaregister-button">Register</button>
+            </div>
+            <!-- <button class="qna-arrow"></button> -->
         </div>
     </div>
 </template>
@@ -19,24 +31,76 @@ export default {
         width: 100%;
         min-height: 500px;
         text-align: center;
-    }
-    .qnacontainer {
-        max-width: 868px;
-        margin: auto;
-        margin-top: 200px;
 
-        h1{
+        h1  {
             font: normal normal 300 40px/55px Montserrat;
+            margin-bottom: 24px;
+        }
+    }
+
+    .qnacontainer {
+        max-width: 700px;
+        margin: auto;
+        margin-top: 180px;
+
+        h1 + p {
+            font-size: 17px;
+            font-weight: 500;
+            margin-bottom: 25px;
         }
 
-        button {
-            padding: 12px 35px;
-            color: rgba(95, 189, 193, 1);
-            background: rgba(95, 189, 193, 0.1);
-            border: none;
-            border-radius: 100px;
-            font-size: 11px;
-            font-weight: 500;
+        .qna-p {
+            font-size: 14px;
+            margin-bottom: 28px;
+            font-weight: 300;
         }
+
+        }
+
+        .qna-questions {
+            background: rgba(23, 10, 109, 1);
+            margin-top: 70px;
+            min-height: 417px;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+
+            h1 {
+                color: white;
+                padding: 45px 0 25px;
+            }
+
+            p {
+                color: white;
+                font-size: 14px;
+                max-width: 540px;
+                margin: auto;
+                margin-bottom: 15px;
+                font: normal normal normal 14px/20px Montserrat;
+                letter-spacing: 0;
+            }
+
+            .qna-arrow {
+                border-radius: 100%;
+                padding: 0;
+                width: 26px;
+                height: 26px;
+                background: white;
+                color: rgba(23, 10, 109, 1);
+                margin: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+        }
+
+        .button {
+            margin: auto;
+        }
+
+        .qaregister-button {
+            margin-top: 30px;
+            margin-bottom: 40px;
         }
 </style>
