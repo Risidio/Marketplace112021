dc2a6ed2d1c36562a83904323e134714a89ea13d
Muhammad Talha Butt <63363869+talhabutt1996@users.noreply.github.com>
2022-04-29 04:24:33 -0700

    uppdateeee

--- src/components/about/AboutSection2.vue
+++ src/components/about/AboutSection2.vue
@@ -35,14 +35,6 @@
           </template>
         </vueper-slide>
       </vueper-slides>
-        <!-- <div>
-          <h1 style="font: normal normal 300 37px/30px Montserrat; color: white; margin-bottom: 50px;">{{content.aboutsection2[0].abouttitle[0].text}}</h1>
-          <p style="color: white; font: normal normal normal 14px/20px Montserrat; max-width: 600px">{{content.aboutsection2[0].abouttext[0].text}}<br><br>
-             {{content.aboutsection2[0].abouttext1[0].text}}</p>
-            <div class="m-5 d-flex justify-content-center">
-              <a title="join us on discord" href="https://discord.com/invite/sQaKVft"><button class="button filled">Read More</button></a>
-            </div>
-        </div> -->
       </div>
   </section>
 </template>
