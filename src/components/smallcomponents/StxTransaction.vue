e5cc82f5a0c3fadb939c0b520fedb5bc9984cecb
Apower11 <59498034+Apower11@users.noreply.github.com>
2022-06-09 09:51:01 +0100

    version 1

--- src/components/smallcomponents/StxTransaction.vue
+++ src/components/smallcomponents/StxTransaction.vue
@@ -3,34 +3,21 @@
         <h3 style="font-size: 13px; font-weight: 700;"> Transactions</h3>
         <table class="transaction-table">
         <tr><th>Date</th><th>Method Type</th><th>By</th><th>Fees</th></tr>
-        <tr class="transaction-data" v-for="(item, index) in stxTransaction" :key="index" @click="openTransaction(item)">
-            <th>{{dayjs(item.burn_block_time_iso).format('DD/MM/YYYY')}}</th>
-            <th v-if="item.contract_call && item.contract_call.function_name"> {{item.contract_call.function_name}}</th> <th v-else> Setup</th>
-            <th class="stx-address">{{item.sender_address.slice(0, 5) + "..." + item.sender_address.slice(-3)}}</th>
-            <th>{{item.fee_rate ? `STX: ${(item.fee_rate/1000000).toFixed(2)} ` : 'N/A'}}</th>
-            <th class="stx-address">{{item.tx_status}}</th>
-        </tr>
+        <StxSingleTransaction class="transaction-data" v-for="(item, index) in stxTransaction" :key="index" :item="item" :index="index"  />
         </table>
     </div>
 </template>
 
 <script>
-import dayjs from 'dayjs'
+import StxSingleTransaction from './StxSingleTransaction.vue'
 
 export default {
   name: 'StxTransaction',
+  components: {
+    StxSingleTransaction
+  },
   props: {
     stxTransaction: Array
-  },
-  data () {
-    return {
-      dayjs: dayjs
-    }
-  },
-  methods: {
-    openTransaction (item) {
-      window.open(`https://explorer.stacks.co/txid/${item.tx_id}`)
-    }
   }
 }
 </script>
--- src/components/smallcomponents/StxTransaction2.vue
+++ src/components/smallcomponents/StxTransaction2.vue
@@ -2,34 +2,27 @@
     <div>
         <table class="transaction-table">
         <tr><th>Date</th><th>Method</th><th>Sold By</th><th>Fees</th><th>Status</th></tr>
-        <tr class="transaction-data" v-for="(item, index) in StxTransaction2" :key="index" @click="openTransaction(item)">
-            <th>{{dayjs(item.burn_block_time_iso).format('DD/MM/YYYY')}}</th>
-            <th v-if="item.contract_call && item.contract_call.function_name"> {{item.contract_call.function_name}}</th> <th v-else> Setup</th>
-            <th class="stx-address">{{item.sender_address.slice(0, 5) + "..." + item.sender_address.slice(-4)}}</th>
-            <th>{{item.fee_rate ? `STX: ${(item.fee_rate/1000000).toFixed(2)} ` : 'N/A'}}</th>
-            <!-- <th>{{item.fee_rate ? `STX: ${item.fee_rate} ` : 'N/A'}}</th> -->
-            <th class="stx-address">{{item.tx_status}}</th>
-        </tr>
+        <StxSingleTransaction class="transaction-data" v-for="(item, index) in stxTransaction2" :key="index" :item="item" :index="index"  />
         </table>
     </div>
 </template>
 
 <script>
 import dayjs from 'dayjs'
+import StxSingleTransaction from './StxSingleTransaction.vue'
 
 export default {
   name: 'StxTransaction2',
+  components: {
+    StxSingleTransaction
+  },
   props: {
     StxTransaction2: Array
   },
   data () {
     return {
-      dayjs: dayjs
-    }
-  },
-  methods: {
-    openTransaction (item) {
-      window.open(`https://explorer.stacks.co/txid/${item.tx_id}`)
+      dayjs: dayjs,
+      authors: []
     }
   }
 }
